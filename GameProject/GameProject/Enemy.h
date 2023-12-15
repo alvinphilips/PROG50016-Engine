bedbf0c1fecb857d61a2f214e64b9032cbf7de83
Alvin Philips <alvinphilips257@gmail.com>
2023-12-15 04:53:29 -0500

    Added base enemy class

---
+++ GameProject/GameProject/Enemy.h
@@ -0,0 +1,26 @@
+#pragma once
+#ifndef ENEMY_H
+#define ENEMY_H
+
+#include "GameCore.h"
+
+class Enemy : public Component
+{
+    DECLARE_DYNAMIC_DERIVED_CLASS(Enemy, Component)
+
+public:
+    void Initialize() override;
+    void Update() override;
+    void Load(json::JSON&) override;
+    void Destroy() override;
+protected:
+    Vec2 previous_position;
+    float speed = 5.0f;
+    TextureAsset* texture = nullptr;
+    CircleCollider* collider = nullptr;
+    Player* player = nullptr;
+};
+
+#endif // ENEMY_H
+
+
